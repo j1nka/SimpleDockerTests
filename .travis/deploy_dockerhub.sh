0fda9e5742ad3a2858586a9208441ed7596da6a1
AG <AnGavrilov8@gmail.com>
2019-08-11 18:23:28 +0300

    Based on materials from https://habr.com/ru/company/southbridge/blog/329262/. Thanks! Great tutorial!

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -2,5 +2,5 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 NAME="simplewebapp:latest"
 docker build -f Dockerfile -t $NAME .
-docker tag "simplewebapp" $DOCKER_USER:$NAME
-docker push $DOCKER_USER:$NAME
+docker tag "simplewebapp" "t0nydocker/"$NAME
+docker push "t0nydocker/"$NAME
