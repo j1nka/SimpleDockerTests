97cb14caa7b09a0fb1826c0927f747803c3af198
AG <AnGavrilov8@gmail.com>
2019-08-11 17:59:46 +0300

    Based on materials from https://habr.com/ru/company/southbridge/blog/329262/. Thanks! Great tutorial!

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 TAG="latest"
-NAME=$TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]'
+NAME="simplewebapp"
 docker build -f Dockerfile -t $NAME:$TAG .
 docker push $TRAVIS_REPO_SLUG
